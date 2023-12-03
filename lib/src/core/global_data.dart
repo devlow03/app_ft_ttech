a5bb29384f61ffbc9967b14724908fe058f36ecd
Quang Thiện <108052745+devlow03@users.noreply.github.com>
2023-12-03 17:38:18 +0700

    Update global_data.dart

--- lib/src/core/global_data.dart
+++ lib/src/core/global_data.dart
@@ -1,17 +1,1 @@
-import 'package:encrypted_shared_preferences/encrypted_shared_preferences.dart';
-
-class GlobalData {
-  GlobalData._();
-
-  static const String baseUrl = 'https://tmart.tuanthanhdev.id.vn/';
-  static const String tikiBanner = 'https://tka.tiki.vn/';
-  static const String tikiProd = 'https://api.tiki.vn/';
-  static const String storeToken = 'iuV0MFNGDdAqwi5o5GRoauKoDMHR5qXsi0W2cMk7Zc2SjqTCGxfMbVDSyBZEUStuPEG53P4';
-  static const String token = 'token';
-  static const String fCmToken = "fCmToken";
-  static const String fCmServerKey = 'Bearer AAAAG0dkn5A:APA91bF5ARlrekqFDajflqDpq3W9HPNeQfj7mrwbVulmtQIXrFx22pobfTXW8jsNuNT2b9Lg9nKiD5dkFHAwfSaq4XbtBHNRYm9CUJPDNVdhdSDBGvg5dl0ZiGiVzSfO03Xp7fdQacvE';
-  static const String userIdNotify = 'userIdNotify';
-  static const String notifyChildKey = 'notifyChildKey';
-
-}
 
